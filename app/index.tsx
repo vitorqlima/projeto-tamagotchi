b8f774b0b2a3e1720ec959a9fa8e8e2f78338ec0
Vitor Lima <vitorqlm@gmail.com>
2024-08-28 22:38:24 -0300

    logica com as caixas dos bichinhos

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,36 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const data = [
+    {title: 'Cadastrar Bichinho'},
+]
+
+type ItemProps = {
+    title: string
+}
+
+const Item = ({title}: ItemProps) => {
+    return(
+        <TouchableOpacity 
+        style={styles.item}
+        onPress={() => {
+            alert("Cadastro iniciado")
+        }}>
+            <Text style={styles.title}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
 
 const index = () => {
-
-
     return (
         <SafeAreaView style={styles.container}>
-            <Image source={require('./icon.png')} style={{ width: 80, height: 80, alignSelf: "center" }} />
+            <Image source={require('../assets/images/icon.png')} style={{ width: 80, height: 80, alignSelf: "center" }} />
             <View style={styles.containerContent}>
+                <FlatList
+                data={data}
+                renderItem={({item}) => <Item title={item.title}/>}
+                horizontal={true}
+                contentContainerStyle={styles.flatListContainer}
+                />
             </View>
         </SafeAreaView>
     );
@@ -21,7 +45,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffdaee',
         flex: 1,
         justifyContent: 'center',
-        alignItems: "center"
+        alignItems: "center",
 
     },
     texto: {
@@ -30,8 +54,24 @@ const styles = StyleSheet.create({
         fontWeight: "300",
         padding: 20,
     },
+    flatListContainer: {
+        justifyContent: 'center',
+        alignItems: "center",
+    },
+    item: {
+        backgroundColor: '#cf51968d',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        width: 250,
+        height: 300,
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      title: {
+        fontSize: 32,
+      },
 
 })
-
 
 export default index;
